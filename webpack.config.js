b6b47558ca5ef7bf3ae6b4fa10132c7bb9c773d4
nampetch <np.sukruansuwan@gmail.com>
2016-10-15 10:43:07 +0700

    Initial setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+    entry: {
+        app: path.join(__dirname, 'src', 'app.js'),
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "app.js"
+    }
+};
+
+module.exports = config;
